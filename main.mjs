b2ee054a8b57e6c59a7fc7b8cb895829d7f6d0bc
imoegirl <imoegirl@outlook.com>
2020-10-08 13:18:29 +0800

    更新逻辑

--- main.mjs
+++ main.mjs
@@ -2,10 +2,37 @@ import "../flyfox/foxcore/foxcore.mjs";
 import PackageHandler from "../flyfox/foxlogic/packages/packages_handler.mjs";
 import HandleMap from "./src/handle_map.mjs";
 import TCPClient from "./src/tcpclient.mjs";
+import Packages from "../flyfox/foxlogic/packages/packages.mjs";
+
+const host = "127.0.0.1";
+const port = 9999;
 
 const packageHandler = new PackageHandler();
+const tcpClient = new TCPClient(host, port);
 
 global.foxLogic = {
   handleMap: HandleMap,
   packageHandler: packageHandler,
+  tcpClient: tcpClient,
 };
+
+const strAddr4G = "000000000012";
+
+tcpClient.connectToServer(
+  () => {
+    global.logger.info("连接成功回调");
+    let onlineMsg = new Packages.SCOnline(strAddr4G);
+    let bytes = onlineMsg.FinishPackage();
+    // global.logger.info(bytes);
+    tcpClient.Send(bytes);
+
+    setInterval(() => {
+      let heartbeat = new Packages.SCHeartBeat(strAddr4G);
+      let bytes = heartbeat.FinishPackage();
+      tcpClient.Send(bytes);
+    }, 2000);
+  },
+  (data) => {
+    global.logger.info("收到数据回调: ", data);
+  }
+);
--- src/tcpclient.mjs
+++ src/tcpclient.mjs
@@ -1,5 +1,36 @@
+import net from "net";
+
 class TCPClient {
-  constructor() {}
+  constructor(host, port) {
+    this.host = host;
+    this.port = port;
+  }
+
+  connectToServer(connectedCallback, dataCallback) {
+    this.socket = net.createConnection(this.port, this.host);
+    this.socket.on("connect", () => {
+      global.netLogger.info("连接服务器成功");
+      connectedCallback();
+    });
+
+    this.socket.on("data", (data) => {
+      global.netLogger.info("收到数据: ", data);
+      dataCallback(data);
+    });
+
+    this.socket.on("close", (data) => {
+      global.netLogger.info("链接断开: ", data);
+    });
+
+    this.socket.on("error", () => {
+      global.netLogger.error("连接出错");
+    });
+  }
+
+  Send(data) {
+    global.netLogger.info("发送数据: ", data);
+    this.socket.write(data);
+  }
 }
 
 export default TCPClient;
